53c69a0dceee92a07e015e758ec6b93f5cd60261
özgür <ozgurpir14@gmail.com>
2024-08-03 17:42:47 +0300

    ordered index players by balance

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   before_action :set_player, only: %i[show edit update destroy transfer withdraw deposit]
 
   def index
-    @players = Player.all
+    @players = Player.order(balance: :desc)
   end
 
   def show
